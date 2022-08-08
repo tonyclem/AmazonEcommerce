4c413313b0ce418d4a293d486211d34de4384d39
tonyclem <tonyclem001@gmail.com>
2022-08-08 22:21:00 +0200

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
