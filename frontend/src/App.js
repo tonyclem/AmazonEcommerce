ab4db41cf223f8e9d84621e39cc71b9ba661136a
tonyclem <tonyclem001@gmail.com>
2022-08-08 23:04:27 +0200

    added images and product data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
       <header>
         <a href="/">amazon</a>
       </header>
-      <main>List Product</main>
+      <main>
+        <h1>List Product</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="product" key={product.slug}>
+                <a href={`/products/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <p>{product.name}</p>
+                  <p>
+                    <strong>{product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
