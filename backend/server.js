2f630c5d015751feb60aa97dbcdebfe6496cf7b4
tonyclem <tonyclem001@gmail.com>
2022-08-09 07:44:22 +0200

    added backend with product

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import data from "./data.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server started on port ${port}`));
