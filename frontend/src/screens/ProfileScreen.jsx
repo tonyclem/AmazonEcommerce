883e67e6210ef6f0e6c032908b83326b16d4e6ef
tonyclem <tonyclem001@gmail.com>
2022-09-15 21:22:16 +0200

    added function, that unable to change password, email

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,111 @@
+import React, { useContext, useState, useReducer } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
+import { Store } from "../Store";
+import { toast } from "react-toastify";
+import axios from "axios";
+import getError from "../utils";
+
+export const ProfileScreen = () => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "UPDATE_REQUEST":
+        return { ...state, loadingUpdate: true };
+      case "UPDATE_SUCCESS":
+        return { ...state, loadingUpdate: false };
+      case "UPDATE_FAIL":
+        return { ...state, loadingUpdate: false };
+      default:
+        return state;
+    }
+  };
+
+  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+    loadingUpdate: false,
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        "/api/users/profile",
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: "UPDATE_SUCCESS",
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      toast.success("User update successful");
+    } catch (error) {
+      dispatch({ type: "FETCH_FAIL" });
+      toast.error(getError(error));
+    }
+  };
+
+  return (
+    <div className="container small-container">
+      <Helmet>
+        <title>User Profile</title>
+      </Helmet>
+      <h1 className="my-3">User Project</h1>
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          ></Form.Control>
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Update</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
